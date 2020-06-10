61879a843d8bef2bae16c738aadb5e46b6081fea
Max Z <max.zlotskiy@gmail.com>
2020-06-10 00:45:22 -0400

    moved app files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+
+import App from './components/containers/App.js';
+import store from './store';
+
 import * as serviceWorker from './serviceWorker';
+import './index.css';
+
+if(module.hot) {module.hot.accept();}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
