6ad057234e969324f65565e180cb98b8d168f4d0
Max Z <max.zlotskiy@gmail.com>
2020-06-10 00:40:05 -0400

    tested connecting to socket-io server

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -1,26 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import io from "socket.io-client";
+import '../../styles/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//global variable to store our client's connection to server
+let socket;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: "main", //main menu, connecting, creating, lobby, game
+      joinCode: ""
+    };
+    //setStatus(DISCONNECTED)
+    console.log("connecting");
+    socket = io("http://localhost:3001/settlers", {
+      rejectUnauthorized: false
+    });
+    socket.on("connection", () => {
+      console.log("connected to server");
+    });
+    socket.on("connect_error", (error) => {
+      console.log("Error connecting to socket");
+      console.log(error);
+    });
+    socket.on("reconnect_error", (error) => {
+      console.log("Reconnection error");
+      console.log(error);
+    });
+    socket.on("reconnect_attempt", (attempt) => {
+      console.log("Attempt " + attempt);
+    });
+    socket.on("disconnect", () => {
+      console.log(socket.id + " disconnecting");
+    });
+    //event handlers need the correct value of "this" bound to them
+    this.handleViewChange = this.handleViewChange.bind(this);
+    this.handleJoinRequest = this.handleJoinRequest.bind(this);
+    this.handleJoinCodeChange = this.handleJoinCodeChange.bind(this);
+  }
+  handleViewChange(view) {
+    this.setState({view: view});
+  }
+  handleJoinRequest(event) {
+    this.setState({view: "connecting"});
+    console.log(this.state.joinCode);
+    //setStatus(CONNECTING)
+    socket.emit("request_join", this.state.joinCode, (joined, playerID) => {
+      console.log("joined: " + joined + " with id: " + playerID);
+      //setStatus(CONNECTED) or DISCONNECTED, msg=refused
+      //setPlayerID(playerID)
+    });
+    //getGameInfo() action -> GET request
+  }
+  handleJoinCodeChange(event) {
+    this.setState({joinCode: event.target.value});
+  }
+  /* React Lifecycle Method. Resources should be cleaned up here. */
+  componentWillUnmount() {
+    socket.disconnect();
+  }
+  render() {
+    const mainMenu = (
+      <Fragment>
+        <h1>Settlers of Marina</h1>
+        <div>
+          <label htmlFor="joinCode">Enter your friend's join code</label>
+          <input type="text" id="joinCode" onChange={this.handleJoinCodeChange} />
+          <button onClick={this.handleJoinRequest}>Join</button>
+        </div>
+        <p>Or...</p>
+        <button>Create Game</button> 
+      </Fragment>
+      );
+    const connecting = (<h1>Connecting...</h1>);
+    const creating = (<h1>Creating Game...</h1>);
+    const lobby = (
+      <div>Lobby</div>
+      );
+    let view;
+    switch(this.state.view) {
+      case "main":
+        view = mainMenu;
+        break;
+      case "connecting":
+        view = connecting;
+        break;
+      case "creating":
+        view = creating;
+        break;
+      case "lobby":
+        view = lobby;
+      default:
+        view = mainMenu; //replace with <Game />
+    }
+    return (
+      <div className="App">{view}</div>
+    );
+  }
 }
 
 export default App;
