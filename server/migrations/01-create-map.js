ee17839df3e41acacbf4a19097a216253cb960b5
Max Z <max.zlotskiy@gmail.com>
2020-06-14 21:46:38 -0400

    created map table

---
+++ server/migrations/01-create-map.js
@@ -0,0 +1,43 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Maps', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        unique: true
+      },
+      max_players: {
+        type: Sequelize.INTEGER
+      },
+      difficulty: {
+        type: Sequelize.INTEGER
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      thumbnail: {
+        type: Sequelize.BLOB
+      },
+      map_data: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Maps');
+  }
+};
---
+++ server/models/map.js
@@ -0,0 +1,18 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Map = sequelize.define('Map', {
+    name: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    max_players: DataTypes.INTEGER,
+    difficulty: DataTypes.INTEGER,
+    description: DataTypes.STRING,
+    thumbnail: DataTypes.BLOB,
+    map_data: DataTypes.STRING
+  }, {});
+  Map.associate = function(models) {
+    // associations can be defined here
+  };
+  return Map;
+};
