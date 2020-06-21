7dca85f0a74c673b76c87064c09d7eb9e952ed18
Max Z <max.zlotskiy@gmail.com>
2020-06-20 20:10:40 -0400

    added constants

--- server/models/player.js
+++ server/models/player.js
@@ -1,14 +1,22 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
+  /* Define constants and utilities */
+  const STATUS = {
+    JOINING: "JOINING",
+    JOINED: "JOINED",
+    DISCONNECTED: "DISCONNECTED"
+  };
+  const PLAYER_COLORS = {
+    0: "#0f0",
+    1: "#00f",
+    2: "#f00",
+    3: "#fff",
+    4: "#a0a",
+    5: "#090"
+  };
+  /* Define database table/model */
   const Player = sequelize.define('Player', {
     socket_id: DataTypes.TEXT,
-    room_code: {
-      type: DataTypes.STRING,
-      references: {
-        model: "Games",
-        key: "join_code"
-      }
-    },
     player_id: DataTypes.INTEGER,
     nickname: DataTypes.STRING,
     color: DataTypes.STRING,
@@ -18,21 +26,24 @@ module.exports = (sequelize, DataTypes) => {
       get: function() { //convert to boolean: 0=false, 1=true
         return this.getDataValue("host") != 0;
       },
-      set: function(value) {
+      set: function(value) { //convert boolean to int
         this.setDataValue("host", value? 1 : 0);
       }
     },
     turn_order: DataTypes.INTEGER
   }, {
-    indexes: {
-      name: "playerInGame",
-      unique: true,
-      fields: ["room_code", "player_id"]
-    }
+    // indexes: {
+    //   name: "playerInGame",
+    //   unique: true,
+    //   fields: ["GameId", "player_id"]
+    // }
   });
   /* Define associations here */
   Player.associate = function(models) {
-    Player.belongsTo(models.Game);
+    Player.belongsTo(models.Game); //Player.GameId references Game.id
   };
+  /* Export constants */
+  Player.STATUS = STATUS;
+  Player.PLAYER_COLORS = PLAYER_COLORS;
   return Player;
 };
