7d0df796f824394c6120364d0b158966f2a6c3cd
Max Z <max.zlotskiy@gmail.com>
2020-06-20 20:11:13 -0400

    added routes for creating and joining a game

--- server/routes/room.js
+++ server/routes/room.js
@@ -1,35 +1,129 @@
 const express = require("express");
 const router = express.Router();
+const {Game, Player, Map, sequelize} = require("../models");
 
-router.get("/game_info", function(req, res, next) {
+/**
+ * GET /api/games/<id>/info
+ * Yeilds a JSON object describing the game if the id is valid.
+ * If the game does not exist, a 404 status is sent.
+ */
+router.get("/:id/info", function(req, res, next) {
 	let id = (req.params.id || "").toUpperCase();
 	//try to find this room in the database
-	if(id == "42AA") {
-		res.json({
-			joinCode: id,
-			status: "CREATED",
-			num_players: 0,
-			max_players: 6,
-			map: "hexagon",
-			seed: 1,
-			map_data: "",
-			longest_road: null,
-			largest_army: null
-		});
+	Game.findByPk(id, {include:[Player, Map]}).then(game => {
+		//found it
+		//add some extra fields for the response
+		game.join_code = game.id;
+		res.json(game);
+	})
+	.catch(err => {
+		//not found
+		res.status(404).send("Game not found");
+	});
+});
+
+/**
+ * PUT /api/games/<id>/join
+ * @request You must have a socketio session cookie
+ * If the game exists, and can fit more players, you will be registered to the
+ * database.
+ */
+router.put("/:id/join", async function(req, res, next) {
+	let id = (req.params.id || "").toUpperCase();
+	//this is the transaction that makes operations more atomic
+	var t;
+	//try to find this room in the database
+	try {
+		//found it
+		const game = await Game.findByPk(id);
+		//if game can't fit more players, stop
+		if(game.num_players >= game.max_players)
+			throw new Error("This game is full");
+		let pid = game.num_players + 1;
+		t = await sequelize.transaction();
+		//add player to game
+		const newPlayer = await Player.create({
+			socket_id: "hello",
+			player_id: pid,
+			nickname: "Player" + pid,
+			color: Player.PLAYER_COLORS[pid],
+			status: Player.STATUS.JOINING,
+			host: false,
+			turn_order: pid
+		}, {transaction: t});
+		await game.addPlayer(newPlayer, {transaction: t});
+		//update the number of players
+		game.set("num_players", game.num_players+1);
+		await game.save({transaction: t});
+		//save changes to database
+		t.commit();
+		//then, respond with {player_id: }
+		res.json(newPlayer.toJSON());
+		//TODO: socketio server tells everyone about new player
 	}
-	//if not found, send a HTTP 404 error code 
-	else {
-		res.status(404);
+	catch(err) {
+		console.log("Error Joining: ", err);
+		if(t) {
+			t.rollback();
+		}
+		res.status(404).send("");
 	}
 });
 
-router.post("/new_game", function(req, res, next) {
-	//generate new ID for game
-	let id = generateJoinCode();
-	//store it in the database
-	//if it succeeded, send join code in response
-	//if it failed, send error response
-	res.status(500);
+router.post("/new_game", async function(req, res, next) {
+	try {
+		let host = sequelize.transaction(async function(t) {
+			//generate new ID for game
+			let id = await generateJoinCode(t);
+			//store game in the database
+			const game = await Game.create({
+				id: id,
+				status: Game.STATUS.LOBBY,
+				status_timestamp: new Date(),
+				num_players: 1,
+				max_players: 4,
+				seed: (Math.random() * 10)|0,
+				deck: Game.DECK,
+			}, {transaction: t});
+			//add 1 player, the host, to this game
+			const host = await game.createPlayer({
+				socket_id: "the host",
+				player_id: 1,
+				nickname: "Player1",
+				color: Player.PLAYER_COLORS[1],
+				status: Player.STATUS.JOINING,
+				host: true,
+				turn_order: 1
+			}, {transaction: t});
+			return host;
+		});
+		//if it succeeded, send join code in response
+		res.json({...host.toJSON(), join_code: host.GameId});
+	} catch(err) {
+		//if it failed, send error response
+		console.log("Failed to create game: ", err);
+		res.status(500).send("");
+	}
 });
+
+/**
+ * Used to create a unique join code for each game.
+ * Customize the following constants:
+ * DIGITS_TOTAL: the length of each join code.
+ * DIGITS_RAND: should be less than DIGITS_TOTAL. It controlls how much influence
+ * time, randomness and the number of games have on the outcome. If it is low,
+ * time has more influence. If it is high, randomness/numberOfGames will have more
+ * influence.
+ * @param t  a sequelize transaction
+ * @return   string
+ */
+async function generateJoinCode(t) {
+	const numGames = await Game.count({transaction: t});
+	const DIGITS_TOTAL = 6;
+	const DIGITS_RAND = 3;
+	let a = numGames.toString(36).slice(DIGITS_RAND - 1);
+	let r = Math.pow(36, DIGITS_RAND - a.length)|0;
+	return a + ((Math.random() * r)|0).toString(36) + Date.now().toString(36).slice(DIGITS_RAND - DIGITS_TOTAL);
+}
 
 module.exports = router;
