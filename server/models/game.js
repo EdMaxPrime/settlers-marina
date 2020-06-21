59600adfe864c72b3b2e16890e7e19060b00d6b8
Max Z <max.zlotskiy@gmail.com>
2020-06-20 20:10:27 -0400

    added default values and constants

--- server/models/game.js
+++ server/models/game.js
@@ -1,5 +1,21 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
+  /* Define constants and utilities here */
+  const PHASE = {
+    SETUP1: "SETUP1",
+    SETUP2: "SETUP2",
+    REGULAR: "REGULAR",
+    SPECIAL: "SPECIAL"
+  };
+  const STATUS = {
+    LOBBY: "CREATED",
+    SETUP: "SETUP",
+    PLAYING: "PLAYING",
+    GAME_OVER: "GAME_OVER"
+  };
+  const DECK = "VVVYY";
+  const NOBODY = -1;
+  /* The actual database table/model */
   const Game = sequelize.define('Game', {
     id: {
       type: DataTypes.STRING,
@@ -7,23 +23,28 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
       autoIncrement: false
     },
-    status: DataTypes.STRING,
-    status_timestamp: DataTypes.DATE,
-    num_players: DataTypes.INTEGER,
-    max_players: DataTypes.INTEGER,
-    turn_now: DataTypes.INTEGER,
-    special_turn: DataTypes.INTEGER,
-    phase: DataTypes.INTEGER,
-    seed: DataTypes.INTEGER,
-    deck: DataTypes.STRING,
-    structures: DataTypes.TEXT,
-    longest_road: DataTypes.INTEGER,
-    largest_army: DataTypes.INTEGER
+    status:       {type: DataTypes.STRING, defaultValue: STATUS.LOBBY},
+    status_timestamp: {type: DataTypes.DATE, defaultValue: new Date()},
+    num_players:  {type: DataTypes.INTEGER, defaultValue: 0},
+    max_players:         DataTypes.INTEGER,
+    turn_now:     {type: DataTypes.INTEGER, defaultValue: NOBODY},
+    special_turn: {type: DataTypes.INTEGER, defaultValue: NOBODY},
+    phase:        {type: DataTypes.INTEGER, defaultValue: PHASE.SETUP1},
+    seed:                DataTypes.INTEGER,
+    deck:                DataTypes.STRING,
+    structures:   {type: DataTypes.TEXT,    defaultValue: ""},
+    longest_road: {type: DataTypes.INTEGER, defaultValue: NOBODY},
+    largest_army: {type: DataTypes.INTEGER, defaultValue: NOBODY}
   }, {});
   /* Define associations here */
   Game.associate = function(models) {
     Game.hasMany(models.Player);
     Game.belongsTo(models.Map);
   };
+  /* Add constants and utilities here */
+  Game.STATUS = STATUS;
+  Game.PHASE = PHASE;
+  Game.DECK = DECK;
+  Game.NOBODY = NOBODY;
   return Game;
 };
