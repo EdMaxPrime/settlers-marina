4d3c10e48c63fe7f3d46dfb0641b26e8b8ef7138
Max Z <max.zlotskiy@gmail.com>
2020-06-27 22:37:00 -0400

    refactored server and websocket structure

--- server/app.js
+++ server/app.js
@@ -6,52 +6,40 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const io = require("socket.io");
+const db = require("./models");
 
-//this is the application/server object
+/***** CONFIGURE EXPRESS APP *****/
 const app = express();
 app.use(bodyParser.json());
 app.use(cookieParser());
-const server = http.createServer(app);
-var socketio = io(server);
-const game = socketio.of("/settlers");
-
 //configure the express app
 //app.use(cors({origin: "http://localhost:3000", credentials: true}));
-
 //socketio.set("origins", "http://localhost:3000");
 
-//this is the socket-io namespace we will be listening on
-game.on("connect", function(socket) {
-	console.log("Socket " + socket.id + " connected to /settlers");
-	socket.on("request_join", (joinCode, callback) => {
-		console.log(socket.id + " wants to join " + joinCode);
-		callback(true, 1);
-	});
-	socket.on("disconnect", () => {
-		console.log(socket.id + " disconnecting");
-	});
-});
-const eventsRouter = require("./websocket");
-eventsRouter(game);
 
-/* Socketio middleware */
-app.use((req, res, next) => {
-	req.settlers = {ns: game, id: "/settlers#" + req.cookies.io};
-	next();
-});
+/***** DECLARE STATIC ROUTES HERE *****/
 
-//Mount our API routes
+/***** EXPRESS SESSION MIDDLEWARE FOR API *****/
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const httpSession    = session({
+	secret: process.env.NODE_APP_SETTLERS_SECRET,
+	store: new SequelizeStore({
+		db: db.sequelize,
+		table: 'Session'
+	}),
+	resave: false,
+	saveUninitialized: false
+})
+app.use(httpSession);
+
+/***** CONFIGURE SOCKETIO EVENTS/ROUTES *****/
+const server = require("./websocket").setup(app, httpSession);
+
+/***** MOUNT API ROUTES *****/
 const apiRouter = require("./routes");
 app.use("/api", apiRouter);
 
-app.get("/", (req, res) => {
-	console.log("GET /");
-	res.send("Hello World\n");
-});
-
-/*
- * ERROR HANDLING
- */
+/***** ERROR HANDLING *****/
 app.use((req, res, next) => {
 	//maybe they tried to access a file that doesn't exist (files have extensions)
 	if(path.extname(req.path).length) {
@@ -65,9 +53,8 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
 	console.log("Caught error");
 	console.error(err);
-	rest.status(err.status || 500).send(err.message || "Internal server error.");
+	res.status(err.status || 500).send(err.message || "Internal server error.");
 });
-/* ERROR HANDLING */
 
 //export the app so it can be used in the www file
 module.exports = server;
