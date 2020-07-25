d14ab16fd403cd57e0fa2e47d5ce9e0cb2dfffac
Max Z <max.zlotskiy@gmail.com>
2020-07-25 05:25:30 -0400

    moved build logic into player class

--- server/websocket/turn.js
+++ server/websocket/turn.js
@@ -55,36 +55,29 @@ server.on("connect", socket => {
 			console.log("[EVENT/skip] error skipping turn ", err);
 		}
 	})
+	/**
+	 * EVENT: build
+	 * This event should be fired on your turn. This is a request to build
+	 * something on the map.
+	 * @request
+	 *   - who: your player_id
+	 *   - what: "Stlm", "City", "Road", "Ship"
+	 *   - where: intersection or edge, the site where this will be built
+	 * @response  true if this is a legal move, false if illegal
+	 * @broadcast  original event IF it was a legal move
+	 */
 	socket.on("build", async (data, response) => {
 		console.log("[EVENT/build] %j", data);
 		try{
 			const player = await auth.myTurn(session);
-			let s = player.Game.structures;
-			let {phase} = player.Game;
-			let canBuild = false;
-			/* To build a settlement, you must meet all the criteria:
-			- during setup:
-			   - intersection doesn't exist or no building there
-			- during normal:
-			   - intersection exists
-			   - no other building there
-			   - you must have a road leading here */
 			if(data.what == "Stlm") {
-				if(phase == "SETUP1" || phase == "SETUP2") {
-					canBuild = !(data.where in s) || (s[data.where].building == null);
-				} else {
-					canBuild = (data.where in s) && (s[data.where].building == null) && (s[data.where].roads.some(r => r[0] == player.player_id));
-				}
-				if(canBuild) {
-					s[data.where] = s[data.where] || {building: null, roads: []};
-					s[data.where].building = [player.player_id, "Stlm"];
-				}
-			}
-			if(canBuild) {
-				await player.updateGame({structures: s});
+				let connected = player.Game.phase.indexOf("SETUP") == -1;
+				await player.buildStlm(data.where, connected, !connected);
 				socket.to(`${player.GameId} players`).emit("build", data); //alert everyone
+				response(true);
+				return;
 			}
-			response(canBuild);
+			response(false);
 		} catch(err) {
 			console.log("Caught error building ", err);
 			response(false);
